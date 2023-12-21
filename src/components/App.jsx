978513ef27397a614c48819fa0a92cea0a1e9c6b
Nazakin <134816178+Nazakin@users.noreply.github.com>
2023-12-21 09:45:22 +0100

    jnasxjnas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+import {ContactForm} from './ContactForm/ContactForm'
+import {ContactList} from './ContactList/ontactList'
+import { Filter } from './Filter/Filter';
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+    filter: '',
+  };
+//
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, number, contacts } = this.state;
+
+    const isNameExist = contacts.some((contact) => contact.name.toLowerCase() === name.toLowerCase());
+
+    if (isNameExist) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+  };
+
+  handleFilterChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      filter: value,
+    });
+  };
+  handleDeleteContact = (contactId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const { name, number, contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+        <div>
+        <h1>Phonebook</h1>
+        <ContactForm name={name} number={number} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+        <Filter filter={filter} handleFilterChange={this.handleFilterChange} />
+        {filteredContacts.length > 0 ? (
+          <ContactList contacts={filteredContacts} onDeleteContact={this.handleDeleteContact} />
+        ) : (
+          <p>No contacts found</p>
+        )}
+    
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import {Contacts} from './Contacts/contacts.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Contacts />
   </React.StrictMode>
 );
